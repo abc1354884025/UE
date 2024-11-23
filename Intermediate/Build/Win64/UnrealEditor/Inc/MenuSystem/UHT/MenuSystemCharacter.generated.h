1727f69169e5774ee630c8758d9fd2ee468a963d
15614802122 <1354884025@qq.com>
2024-11-23 16:09:00 +0800

    添加项目文件。

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/MenuSystem/UHT/MenuSystemCharacter.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "MenuSystemCharacter.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MENUSYSTEM_MenuSystemCharacter_generated_h
+#error "MenuSystemCharacter.generated.h already included, missing '#pragma once' in MenuSystemCharacter.h"
+#endif
+#define MENUSYSTEM_MenuSystemCharacter_generated_h
+
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_SPARSE_DATA
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_ACCESSORS
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMenuSystemCharacter(); \
+	friend struct Z_Construct_UClass_AMenuSystemCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AMenuSystemCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MenuSystem"), NO_API) \
+	DECLARE_SERIALIZER(AMenuSystemCharacter)
+
+
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMenuSystemCharacter(AMenuSystemCharacter&&); \
+	NO_API AMenuSystemCharacter(const AMenuSystemCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMenuSystemCharacter); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMenuSystemCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMenuSystemCharacter) \
+	NO_API virtual ~AMenuSystemCharacter();
+
+
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_18_PROLOG
+#define FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_SPARSE_DATA \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_ACCESSORS \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_INCLASS_NO_PURE_DECLS \
+	FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h_21_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MENUSYSTEM_API UClass* StaticClass<class AMenuSystemCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_UEPro_MenuSystem_Source_MenuSystem_MenuSystemCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
